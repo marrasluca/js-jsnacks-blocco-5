1bf530bc81ea43174eb065942e4f032e1ac0ad0e
marrasluca <lmarras10@gmail.com>
2025-01-27 15:05:41 +0100

    add: snacks 1-2

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,10 @@
+
+
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+names.forEach(names => {
+    console.log (names)
+})
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
